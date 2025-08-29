16041eeb3c9545003f64db4f18234e3b07a7120c
Mehedi Hassan <mhkmehedihassan@gmail.com>
2025-08-29 12:22:30 +0600

    make coppy button and clear btn

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
 const historydata = [];
- function cardAlert(id,serviceName,serviceNumber) {
-console.log("Call button clicked");
-const CoinCount = parseInt(document.getElementById(id).innerText);
-if (CoinCount<20){
-alert(`you have not sufficient coin ,you have to call atlist 20 coin need`)
-}
-else{
-alert( `calling ${serviceName} ${serviceNumber}`);
- const totalCoinCount = parseInt(CoinCount-20);
-    document.getElementById(id).innerText = totalCoinCount  ;
-}
+function cardAlert(id, serviceName, serviceNumber) {
+  console.log("Call button clicked");
+  const CoinCount = parseInt(document.getElementById(id).innerText);
+  if (CoinCount < 20) {
+    alert(`you have not sufficient coin ,you have to call atlist 20 coin need`);
+  } else {
+    alert(`calling ${serviceName} ${serviceNumber}`);
+    const totalCoinCount = parseInt(CoinCount - 20);
+    document.getElementById(id).innerText = totalCoinCount;
+  }
 }
 
 // function make for history
-function historyobj(serviceName ,serviceNumber){
+function historyobj(serviceName, serviceNumber) {
+  const data = {
+    name: serviceName,
+    Number: serviceNumber,
+    date: new Date().toLocaleString(),
+  };
 
-const data = {
-  name: "serviceName" ,
-  Number:"serviceNumber" ,
-  date: new Date().toLocaleString()
- 
-};
+  historydata.push(data);
+  // function EmergencyService(id, serviceName, serviceNumber)
+  function EmergencyService(id, serviceName, serviceNumber) {
+    const callbtns = document.getElementsByClassName(id);
+    for (const callbtn of callbtns) {
+      callbtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.getElementById("history-Container").style.display = "block";
 
-historydata.push(data);
+        const naturalAlert = cardAlert(
+          "coin-count",
+          serviceName,
+          serviceNumber
+        );
+        const historyObj = historyobj(serviceName, serviceNumber);
 
-
-
+        const createHistory = historyCreate(
+          id,
+          serviceName,
+          serviceNumber
+        );
+      });
+    }
+  }
 }
-// function abc()
-// {
- const hcallbtns= document.getElementsByClassName("call");
-for (const hcallbtn of hcallbtns) {
-  hcallbtn.addEventListener("click", function (e) {
-    e.preventDefault();
+function historyCreate(id, serviceName, serviceNumber) {
+  const hcallbtns = document.getElementsByClassName(id);
+  for (const hcallbtn of hcallbtns) {
+    hcallbtn.addEventListener("click", function (e) {
+      e.preventDefault();
 
-const historyContainer = document.getElementById("history-Container")
- historyContainer.innerText = ""
-for (const data of historydata)
- { console.log("ban")
-const div = document.createElement("div");
-  div.innerHTML = `<div class="flex justify-between items-center bg-[#fafafa] p-4 rounded-2">
-              <div><h1 class="font-bold">Fire Service Number</h1> 
-                   <p class="text-[#5c5c5c]">999</p>
+      const historyContainer = document.getElementById("history-Container");
+      historyContainer.innerText = "";
+      for (const data of historydata) {
+        console.log("ban");
+        const div = document.createElement("div");
+        div.innerHTML = `<div class=" lg:flex justify-between items-center bg-[#fafafa] p-4 rounded-2">
+              <div><h1 class="font-bold">${data.name}</h1> 
+                   <p class="text-[#5c5c5c]">${data.Number}</p>
               </div>
-              <div>date</div>
-            </div>`
- historyContainer.appendChild(div);
+              <div>${data.date}</div>
+            </div>`;
+        historyContainer.appendChild(div);
+      }
+    });
+  }
 }
-});
-}
-
-// }
-
 
 // function history(id1,id2) {
 //   document.getElementById(call).addEventListener("click",function(){
@@ -60,7 +74,7 @@ const div = document.createElement("div");
 // for (const data of historydata) {
 //   const div = document.createElement("div");
 //   div.innerHTML = `<div class="flex justify-between items-center bg-[#fafafa] p-4 rounded-2">
-//               <div><h1 class="font-bold">Fire Service Number</h1> 
+//               <div><h1 class="font-bold">Fire Service Number</h1>
 //                    <p class="text-[#5c5c5c]">999</p>
 //               </div>
 //               <div>date</div>
@@ -69,35 +83,67 @@ const div = document.createElement("div");
 // }
 // })
 // }
-// heart-count 
+// heart-count
 // and love increase
 console.log(historydata);
 
- const hearts= document.getElementsByClassName("heart");
+const hearts = document.getElementsByClassName("heart");
 for (const heart of hearts) {
   heart.addEventListener("click", function (e) {
     e.preventDefault();
     const loveCount = parseInt(document.getElementById("love-count").innerText);
 
-   const totalLoveCount = parseInt(loveCount+1);
-    document.getElementById("love-count").innerText = totalLoveCount  ;
+    const totalLoveCount = parseInt(loveCount + 1);
+    document.getElementById("love-count").innerText = totalLoveCount;
   });
 }
+// copyCounts
+const copys = document.getElementsByClassName("copy-btn");
+
+for (const copy of copys) {
+  copy.addEventListener("click", function (e) {
+    e.preventDefault();
+    const copyCount = parseInt(document.getElementById("copy-count").innerText);
+
+    const totalCopyCount = parseInt(copyCount + 1);
+    document.getElementById("copy-count").innerText = totalCopyCount;
+  });
+}
+
+
+
 // card 1
 
- const callbtns= document.getElementsByClassName("call");
+const callbtns = document.getElementsByClassName("call-1");
 for (const callbtn of callbtns) {
   callbtn.addEventListener("click", function (e) {
     e.preventDefault();
+    document.getElementById("history-Container").style.display = "block";
 
+    const naturalAlert = cardAlert(
+      "coin-count",
+      "National Emergency Number",
+      999
+    );
+    const historyObj = historyobj("National Emergency Number", 999);
 
-   const naturalAlert = cardAlert("coin-count","National Emergency Number",999)
-const historyObj = historyobj("National Emergency Number",999)
-    
-
-
-  
+    const createHistory = historyCreate(
+      "call-1",
+      "National Emergency Number",
+      999
+    );
   });
 }
+// const card1 = EmergencyService("call-1", "National Emergency Number", "999");
 
+// toggle
 
+const clearbtns = document.getElementById("clear");
+
+clearbtns.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  document.getElementById("history-Container").innerText = "";
+});
+
+document.getElementById("history-Container").style.display = "none";
